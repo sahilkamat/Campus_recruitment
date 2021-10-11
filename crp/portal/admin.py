a66e0f7518e416abbfb223d406e19b610bd82cfb
Sriyog Dixit <sriyog.dixit@gmail.com>
2021-10-11 16:49:42 +0530

    Initial commit

---
+++ crp/portal/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from portal.models import StudentDetails, CompanyDetails, JobPosition, AppliedJobs
+admin.site.register(StudentDetails)
+admin.site.register(CompanyDetails)
+admin.site.register(JobPosition)
+admin.site.register(AppliedJobs)
