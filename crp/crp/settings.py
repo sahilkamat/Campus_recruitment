7b67ab50a79d30b39a07a7c13e56e4db8541340c
rishabh-vasudevan <rishabhvasudevan19@gmail.com>
2021-11-25 17:42:55 +0530

    debugging

--- crp/crp/settings.py
+++ crp/crp/settings.py
@@ -135,7 +135,7 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'), )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 
